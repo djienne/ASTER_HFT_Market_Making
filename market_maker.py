34668b0cc7366e7059e43df6ed2c37312379a3b6
djienne <89462218+djienne@users.noreply.github.com>
2025-09-25 00:06:00 +0200

    Update market_maker.py

--- market_maker.py
+++ market_maker.py
@@ -19,7 +19,7 @@ DEFAULT_BALANCE_FRACTION = 0.2  # Use fraction of available balance for each ord
 POSITION_THRESHOLD_USD = 15.0  # Fixed USD value threshold for position closure
 
 # TIMING (in seconds)
-ORDER_REFRESH_INTERVAL = 1    # How long to wait before cancelling an unfilled order.
+ORDER_REFRESH_INTERVAL = 1    # How long to wait before cancelling an unfilled order (seconds).
 RETRY_ON_ERROR_INTERVAL = 30    # How long to wait after a major error before retrying.
 PRICE_REPORT_INTERVAL = 60      # How often to report current prices and spread to terminal.
 BALANCE_REPORT_INTERVAL = 60    # How often to report account balance to terminal.
